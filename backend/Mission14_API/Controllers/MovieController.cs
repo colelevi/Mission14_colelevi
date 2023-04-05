3568a4f07412b1e993d43c755cc68e5c43aa8cdb
colelevi <christensencole4@gmail.com>
2023-04-05 15:48:12 -0600

    commit

---
+++ backend/Mission14_API/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission14_API.Data;
+using System.Linq;
+
+namespace Mission14_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+
+        public MovieController (MovieDbContext temp)
+        {
+            context = temp;
+        }
+        
+        public IEnumerable<MovieCollection> Get()
+        {
+            return (context.Movies.ToArray()
+                .Where(x => x.Edited == "Yes")
+                .OrderBy(x => x.Title));
+        }
+
+    }
+}
